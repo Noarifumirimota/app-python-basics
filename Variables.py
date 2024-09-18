da622428fa0ae6507bc6613c84a81bcaef902648
Szymon Sroka <sroka.a.szymon@outlook.com>
2024-09-18 23:13:58 +0200

    feat: Add operations on variables

---
+++ Variables.py
@@ -0,0 +1,28 @@
+x = 10
+
+x = x + 5
+x += 5
+print(x)
+
+x = x - 2
+x -= 2
+print(x)
+
+x = x * 2
+x *= 2
+print(x)
+
+x = x / 4
+x /= 4
+print(x)
+
+y = 6
+x = x + y
+x += y
+print(x)
+
+x = pow(x, 2)
+print('pow: ' + str(x))
+
+x = pow(16, 2) * 2
+print('pow * 2: ', str(x))
