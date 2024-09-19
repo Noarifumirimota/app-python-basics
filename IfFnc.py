c17e9f68fe389d40f2f7b097fd7ad11e9edb49bf
Szymon Sroka <sroka.a.szymon@outlook.com>
2024-09-19 18:48:41 +0200

    feat: Add if fnc and modulo check

---
+++ IfFnc.py
@@ -0,0 +1,16 @@
+x = 3
+
+if x > 5:
+    print('x > 5')
+elif x == 5:
+    print('x == 5')
+else:
+    print('else!')
+
+print('\nModulo\n')
+
+y = 81
+if(y % 3 == 0):
+    print('Yes!')
+else:
+    print('No :/')
